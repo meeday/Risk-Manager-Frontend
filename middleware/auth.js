7433d5f98c85317d2888688fca24e1e91e5bed03
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-08-14 01:11:33 +0100

    authentication created

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,3 +2,20 @@ const passport = require('passport')
 const localStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt');
 const User = require('../db/models/user');
+
+
+
+//  authenticated local strategy using username and passwod
+passport.use(new localStrategy((username, password, done) =>{
+    User.findOne({username}, (err, user) => {
+        // something went wrong with database
+        if(err)
+        return done(err);
+        // if no user exist
+        if(!user)
+        return done(null, false);
+        //if password if correct
+        user.comparePassword(password, done);
+
+    });
+}));
