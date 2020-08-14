557c68acd194cb181621e9842325a3d69fd97b86
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-08-14 01:12:30 +0100

    authorization created

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,26 @@ const localStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt');
 const User = require('../db/models/user');
 
-
+// this is authorization.
+// once we  areauthenticated we setting up a cookie user's browser, this cookie will be our jwt-Token
+passport.use(new JwtStrategy({
+    jwtFromRequest: cookieExtrator,
+    //this key will verify is this token is legilimen
+    secretOrKey: 'riskManager'
+},(payload, done) => {
+    User.findById({_id: payload.sub}, (err, user) => {
+        // if there is any error
+        if(err)
+        return done(err, false);
+        // this user is already authenticated, we dont have to check the password again
+        if(user)
+        // if user exist no err return user
+        return done(null, user);
+        else
+        // there is no user has that primary key
+        return done(null, false)
+    })
+}))
 
 //  authenticated local strategy using username and passwod
 passport.use(new localStrategy((username, password, done) =>{
