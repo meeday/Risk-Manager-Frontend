66811c71620d3b6d5f5a8881028d8f3e42c8a843
meeday <59993824+meeday@users.noreply.github.com>
2020-08-14 11:42:07 +0100

    userform to hold signin/up component

---
+++ src/components/userForm/userForm.js
@@ -0,0 +1,65 @@
+import React from "react";
+import SignIn from "../signIn/SignIn";
+import SignUp from "../signUp/signUp";
+import "./userForm.css";
+
+class UserForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { form: "login" };
+
+    // I like using objects to toggle values. We could just use true/false and just set to !self value as well.
+    this.toggle = {
+      login: "register",
+      register: "login",
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <div
+            onSubmit={this.onSubmit.bind(this)}
+            style={{
+              transform: `translate(${
+                this.state.form === "login" ? -50 : 200
+              }px, 0px)`,
+            }}
+            className="form-div"
+          >
+            {this.state.form === "login" ? <SignIn /> : <SignUp />}
+          </div>
+          <div className="toggle"
+            style={{
+              transform: `translate(${
+                this.state.form === "login" ? 0 : -200
+              }px, 0px)`,
+            }}
+            className="button-div"
+          >
+            <p>
+              {this.state.form === "login"
+                ? "Do not have an account?"
+                : "Already a member?"}
+            </p>
+            <button
+              onClick={() => {
+                this.setState({ form: this.toggle[this.state.form] });
+              }}
+            >
+              {this.toggle[this.state.form]}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserForm;
