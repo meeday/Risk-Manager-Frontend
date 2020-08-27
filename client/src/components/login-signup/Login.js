4f27fe16a63a51c79debed9c2a9e869d32ebb224
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-08-27 18:27:20 +0100

    Login component created

--- client/src/components/login-signup/Login.js
+++ client/src/components/login-signup/Login.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import AuthService from '../../Services/AuthService';
+import Message from '../Message/Message';
+import {AuthContext} from '../../Context/AuthContext';
 import { useForm } from "react-hook-form";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default function Login() {
+export default function Login(props) {
   const { register, handleSubmit } = useForm();
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [message, setMessage] = useState(null);
+  const authContext = useContext(AuthContext);
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = (user, e) => {
+    e.preventDefault();
+    AuthService.login(user).then(data => {
+        const {isAuthenticated, user, message} = data;
+        if(isAuthenticated){
+          // <AuthContext.Provider value={ data } />;
+          // authContext.setIsAuthenticated(isAuthenticated);
+          //   this history object from react-router
+          // if authenticated navigate to Home  
+          props.history.push('/home')
+        }
+        else{
+          // this will be an error message telling whats wrong
+          setMessage(message);
+        }
+    });
   };
 
   return (
+    
+    <div>
+      {message ? <Message message={message}/> : null}
     <form onSubmit={handleSubmit(onSubmit)}>
       <h3>Sign In</h3>
 
       <div className="form-group">
         <label>Email address</label>
         <input
-          name="email"
+          name="username"
           type="email"
           className="form-control"
           placeholder="Enter email"
@@ -52,6 +75,7 @@ export default function Login() {
           </li>
         </ul>
       </div>
-    </form>
+    </form>    
+    </div>
   );
 }
