db8f80078e892962a9ccd3d74f1922419a16280c
meeday <59993824+meeday@users.noreply.github.com>
2020-08-31 18:58:09 +0100

    account dashboard component complete

--- src/components/pages/AccountDashboard/AccountDashboard.js
+++ src/components/pages/AccountDashboard/AccountDashboard.js
@@ -1,24 +1,47 @@
 import React from "react";
-import Navbar from "../../Nav/Nav";
-import "../../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Avatar from "./worker.png";
 import "./styles/AccountDashboard.css";
 
-function AccountDashboard(props) {
+function AccountDashboard() {
   return (
-    <>
-      <Navbar />
-      <div>
-        <h1>Account details</h1>
-        <h2>Welcome, firstName lastName</h2>
-        <p>Job title: ....</p>
-        <p>Email: ....</p>
-        <p>Company: ....</p>
-
-        <button>
-          <a>Edit details</a>
-        </button>
+      <div className="profile container-fluid">
+        <div className="profile__picture">
+          <img src={Avatar} alt="avatar" />
+        </div>
+        <div className="profile__header">
+          <div className="profile__account">
+            <h4 className="profile__username">Meedaxa Ahmed</h4>
+          </div>
+        </div>
+        <div className="profile__stats">
+          <div className="profile__stat">
+            <div className="profile__icon profile__icon--green">
+              <i className="material-icons">engineering</i>
+            </div>
+            <div className="profile__value">
+              11
+              <div className="profile__key">Projects</div>
+            </div>
+          </div>
+          <div className="profile__stat">
+            <div className="profile__icon profile__icon--red">
+              <i className="fas fa-exclamation-circle"></i>
+            </div>
+            <div className="profile__value">
+              38
+              <div className="profile__key">Issues</div>
+            </div>
+          </div>
+          <div className="profile__stat">
+            <div className="profile__icon profile__icon--blue">
+              <i className="fas fa-comments"></i>
+            </div>
+            <div className="profile__value">
+              18<div className="profile__key">Comments</div>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
   );
 }
 
