d53f99ccb2ad9d1640894acb1ec3148c4d353c2b
meeday <59993824+meeday@users.noreply.github.com>
2020-08-31 16:50:29 +0100

    Navbar imported into dashboard

--- src/components/pages/AccountDashboard/AccountDashboard.js
+++ src/components/pages/AccountDashboard/AccountDashboard.js
@@ -1,21 +1,24 @@
 import React from "react";
+import Navbar from "../../Nav/Nav";
 import "../../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./styles/AccountDashboard.css";
 
 function AccountDashboard(props) {
   return (
-    <div>
-      <h1>Account details</h1>
-      <h2>Welcome, firstName lastName</h2>
-      <p>Job title: ....</p>
-      <p>Email: ....</p>
-      <p>Company: ....</p>
+    <>
+      <Navbar />
+      <div>
+        <h1>Account details</h1>
+        <h2>Welcome, firstName lastName</h2>
+        <p>Job title: ....</p>
+        <p>Email: ....</p>
+        <p>Company: ....</p>
 
-      <button>
-        <a>Edit details</a>
-      </button>
-
-    </div>
+        <button>
+          <a>Edit details</a>
+        </button>
+      </div>
+    </>
   );
 }
 
