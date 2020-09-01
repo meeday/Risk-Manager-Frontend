ccf452dfe22a130e224c6f23a08dc552160290ef
meeday <59993824+meeday@users.noreply.github.com>
2020-09-01 16:45:35 +0100

    add project button added

--- src/components/ProjectsList/ProjectList.js
+++ src/components/ProjectsList/ProjectList.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { List, LinkItem } from "../List/List";
 import "./ProjectList.css";
-const projects = ["project 1", "project 2", "project 3", "project 4", "project 5"];
+import UserForm from "../userForm/UserForm";
+const projects = [
+  "project 1",
+  "project 2",
+  "project 3",
+  "project 4",
+  "project 5",
+];
 export default function ProjectList() {
   return (
     <div className="projectList">
       <List className="list">
         {projects.map((project) => (
-          <LinkItem className="listItem" href={project} key={project}>
+          <LinkItem className="listItem btn btn-primary" href={project} key={project}>
             {project}
           </LinkItem>
         ))}
       </List>
+      <div className="text-center">
+        <button type="submit" className="btn btn-primary add">
+          Add Project
+        </button>
+      </div>
     </div>
   );
 }
