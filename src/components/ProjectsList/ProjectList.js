1438271823fae7df4342efce048213a1060c4701
meeday <59993824+meeday@users.noreply.github.com>
2020-09-02 15:17:46 +0100

    project array edited

--- src/components/ProjectsList/ProjectList.js
+++ src/components/ProjectsList/ProjectList.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { List, LinkItem } from "../List/List";
 import "./ProjectList.css";
-const projects = [
-  "project 1",
-  "project 2",
-  "project 3",
-  "project 4",
-  "project 5",
-];
+const projects = [ {_id: "234iuhowef9803rh", title: "Big Ben" }, {_id: "584pduablf9571qt", title: "London Bridge" } ]
 export default function ProjectList() {
   return (
     <div className="projectList">
       <List className="list">
         {projects.map((project) => (
-          <LinkItem className="listItem btn btn-primary" href={`project-${project.id}`} key={project._id}>
+          <LinkItem className="listItem btn btn-primary" href={"project/" + project._id} key={project._id}>
            {project.title}
           </LinkItem>
         ))}
