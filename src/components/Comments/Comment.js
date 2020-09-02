fd7fec6710651009b461ea7658e6bdeb2eaac613
meeday <59993824+meeday@users.noreply.github.com>
2020-09-02 15:06:35 +0100

    class names changed

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -4,41 +4,41 @@ import "./comment.css";
 export default function Comment() {
   return (
     <div className="comments-div">
-      <div className="section-comments__comments">
-        <h6 className="comments__title">Comments</h6>
-        <ul className="comments__list-comment">
-          <li className="list-comment__comment">
+      <div className="comments-section">
+        <h6 className="comments-title">Comments</h6>
+        <ul className="comments-list">
+          <li className="list-item">
           
-            <div className="comment__comment-text">
-              <h5 className="comment-text__name-author text-center">
+            <div className="comment-text">
+              <h5 className="name-author">
                 joe Dodgson
-                <span className="name-author__data-post text-center">3 hours ago</span>
+                <span className="data-post">3 hours ago</span>
               </h5>
-              <p className="comment-text__content text-center">
+              <p className="comment-content">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               </p>
             </div>
           </li>
-          <li className="list-comment__comment">
+          <li className="list-item">
             
-            <div className="comment__comment-text">
-              <h5 className="comment-text__name-author text-center">
+            <div className="comment-text">
+              <h5 className="name-author">
                 Niro
-                <span className="name-author__data-post text-center"> 2 hours ago</span>
+                <span className="data-post "> 2 hours ago</span>
               </h5>
-              <p className="comment-text__content text-center">
+              <p className="comment-content ">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Voluptates explicabo enim amet.
               </p>
             </div>
           </li>
-          <li className="list-comment__comment">
-            <div className="comment__comment-text">
-              <h5 className="comment-text__name-author text-center">
+          <li className="list-item">
+            <div className="comment-text">
+              <h5 className="name-author ">
                 Ian
-                <span className="name-author__data-post text-center"> 4 hours ago</span>
+                <span className="data-post "> 4 hours ago</span>
               </h5>
-              <p className="comment-text__content text-center">
+              <p className="comment-content ">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               </p>
             </div>
