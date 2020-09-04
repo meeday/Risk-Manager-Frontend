57749316a98c88f4f5ecdbd6873bc3dda60ed9f8
Joe Dodgson <j.dodgson@live.co.uk>
2020-09-04 23:13:58 +0100

    Updated list and projectList to fix href

--- src/components/ProjectsList/ProjectsList.js
+++ src/components/ProjectsList/ProjectsList.js
@@ -2,14 +2,16 @@ import React from "react";
 import { List, LinkItem } from "../List/List";
 import "./ProjectsList.css";
 const projects = [ {_id: "234iuhowef9803rh", title: "Big Ben" }, {_id: "584pduablf9571qt", title: "London Bridge" } ]
-export default function ProjectList() {
+export default function ProjectList(props) {
   return (
     <div className="projectList">
       <List className="list">
         {projects.map((project) => (
-          <LinkItem className="listItem btn btn-primary" href={"project/" + project._id} key={project._id}>
-           {project.title}
-          </LinkItem>
+          <a href={"project/" + project._id}>
+            <LinkItem className="listItem btn btn-primary" key={project._id}>
+            {project.title}
+            </LinkItem>
+          </a>
         ))}
       </List>
       <div className="text-center">
