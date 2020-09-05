6cf6b45df5ab7a1b633acf6ee1173eb2b179237c
Joe Dodgson <j.dodgson@live.co.uk>
2020-09-05 14:15:21 +0100

    Changed AuthService to async await

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -2,52 +2,63 @@
 
 //creating all the fetch request for end-points
 export default {
-    register : user =>{
-        return fetch(`/api/user/register`, {
-            method: "post",
-            body: JSON.stringify(user),
-            headers : {
-                'Content-Type' : 'application/json'
-            }
-        }).then(res => res.json())
-        .then(data => data);
-    },
-
-    login : user =>{
-        return fetch(`/api/user/login`, {
-            method: "post",
-            credentials: "include",
-            body: JSON.stringify(user),
-            headers : {
-                'Content-Type' : 'application/json'
-            }
-        }).then(res => {
-            // passport send 401 status if user not authenticated
-                if(res.status !==401)
-                return res.json().then(data => data)
-                else
-                return{isAuthenticated : false, user: {email: ''}}
+    register : async user => {
+        try {
+            const res = await fetch(`/api/user/register`, {
+                method: "post",
+                body: JSON.stringify(user),
+                headers : {
+                    'Content-Type' : 'application/json'
+                }
             });
+            return res.json();
+        }
+        catch (error) {
+            console.log(`Error - AuthService.js - register() - ${error}`)
+        }
+    },
+    
+    login : async user => {
+        try {
+            const res = await fetch(`/api/user/login`, {
+                method: "post",
+                credentials: "include",
+                body: JSON.stringify(user),
+                headers : {
+                    'Content-Type' : 'application/json'
+                }
+            });
+            return res.json();
+        }
+        catch (error) {
+            console.log(`Error - AuthService.js - login() - ${error}`)
+        }
     },
 
-    logout : () => {
-        return fetch(`/api/user/logout`)
-            .then(res => res.json())
-            .then(data => data);
+    logout : async () => {
+        try {
+            const res = await fetch(`/api/user/logout`);
+            return res.json();
+        }
+        catch (error) {
+            console.log(`Error - AuthService.js - logout() - ${error}`)
+        }
     },
     // this isAuthenticated function use to persist authentican
     // once user login state in the react app will know user has been authenticated, but when user close the app the state will gone.
     // this function will sinc front-end and back-end and keep user authenticated even react app is closed.
     // so, when user visit the website next time user will still stay login
     //we use context-API to call this function, it is a global state for our react app   
-    isAuthenticated : () => {
-        return fetch (`/api/user/authenticated`)
-            .then(res => {
-            // passport send 401 status if user not authenticated
-                if(res.status !==401)
+    isAuthenticated : async () => {
+        try {
+            const res = await fetch (`/api/user/authenticated`);
+            if(res.status !==401)
                 return res.json().then(data => data)
-                else
+            else
                 return{isAuthenticated : false, user: {email: ''}}
-            })
+        }
+        catch (error) {
+            console.log(`Error - AuthService.js - isAuthenticated() - ${error}`)
+        }
     }
 }
