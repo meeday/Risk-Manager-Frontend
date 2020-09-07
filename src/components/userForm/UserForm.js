481e2ab6ddd66f215e86030aef678aa48e4ce96d
Joe Dodgson <j.dodgson@live.co.uk>
2020-09-07 22:04:50 +0100

    Corrected name of Userform.css and .js

---
+++ src/components/userForm/UserForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Logo from "../../static/images/Logo.png";
+import "./styles/UserForm.css";
+
+function UserForm(props) {
+  return (
+    <div>
+      <div className="text-center">
+        <img className="logo" src={Logo} alt="logo" />
+      </div>
+      <div>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            {props.children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserForm;
