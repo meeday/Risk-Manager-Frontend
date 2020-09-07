906311e341bab8b63e3f51d61ce7fbe4613520f3
Joe Dodgson <j.dodgson@live.co.uk>
2020-09-07 22:45:03 +0100

    Amended styling of comments

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -6,44 +6,41 @@ export default function Comment() {
     <div className="comments-div">
       <div className="comments-section">
         <h6 className="comments-title">Comments</h6>
-        <ul className="comments-list">
-          <li className="list-item">
+        <div className="list-item">
+          <div className="comment-text">
+            <h5 className="name-author">
+              joe Dodgson
+              <span className="data-post">3 hours ago</span>
+            </h5>
+            <p className="comment-content">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div className="list-item">
           
-            <div className="comment-text">
-              <h5 className="name-author">
-                joe Dodgson
-                <span className="data-post">3 hours ago</span>
-              </h5>
-              <p className="comment-content">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              </p>
-            </div>
-          </li>
-          <li className="list-item">
-            
-            <div className="comment-text">
-              <h5 className="name-author">
-                Niro
-                <span className="data-post "> 2 hours ago</span>
-              </h5>
-              <p className="comment-content ">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptates explicabo enim amet.
-              </p>
-            </div>
-          </li>
-          <li className="list-item">
-            <div className="comment-text">
-              <h5 className="name-author ">
-                Ian
-                <span className="data-post "> 4 hours ago</span>
-              </h5>
-              <p className="comment-content ">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              </p>
-            </div>
-          </li>
-        </ul>
+          <div className="comment-text">
+            <h5 className="name-author">
+              Niro
+              <span className="data-post "> 2 hours ago</span>
+            </h5>
+            <p className="comment-content ">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptates explicabo enim amet.
+            </p>
+          </div>
+        </div>
+        <div className="list-item">
+          <div className="comment-text">
+            <h5 className="name-author ">
+              Ian
+              <span className="data-post "> 4 hours ago</span>
+            </h5>
+            <p className="comment-content ">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
