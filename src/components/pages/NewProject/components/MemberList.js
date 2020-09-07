38406fdd73b4bc09f903a99e68701a2e8a3c9969
ian <chengkwanyu@yahoo.com.hk>
2020-09-07 22:14:13 +0800

    Get request for user from DB

--- src/components/pages/NewProject/components/MemberList.js
+++ src/components/pages/NewProject/components/MemberList.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Dropdown, Form } from "react-bootstrap";
 
 // API get route
 import userService from "../../../../Services/UserService";
 
 function MemberList(props) {
-  const teamMember = userService.getAllUser;
-  //const teamMember = ["ian", "alex", "mary"];
+  const [teamMember, setTeamMember] = useState([]);
+  const getTeamMember = async () => {
+    try {
+      const dataReturn = await userService.getAllUser();
+      const arrayData = dataReturn.data.UserData;
+  
+      arrayData.map(user => {
+        const test =
+          {
+            userName: `${user.firstName} ${user.lastName}`,
+            userId: user._id
+          }
+        setTeamMember(oldList => [...oldList, test]);
+      });
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getTeamMember();
+  }, []);
+
   return (
     <Dropdown>
       <Dropdown.Toggle variant="light" id="dropdown-basic">
-        ---Choose---
+        Select
       </Dropdown.Toggle>
       <Dropdown.Menu>
         {teamMember.map(function (member, index) {
@@ -18,11 +40,11 @@ function MemberList(props) {
             <div className="row" key={index} style={{ marginLeft: 5 }}>
               <Form.Check
                 type="checkbox"
-                name={member}
-                id={index}
+                name={member.userName}
+                id={member.userId}
                 onChange={props.onchange}
               />
-              <p>{member}</p>
+              <p>{member.userName}</p>
             </div>
           );
         })}
