fbba4fac2b3506e49a5cdf8e88b3bd14c81bc514
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-09-09 22:20:30 +0100

    rendered a list of project according to user

--- src/components/ProjectsList/ProjectsList.js
+++ src/components/ProjectsList/ProjectsList.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { List, LinkItem } from "../List/List";
 import "./ProjectsList.css";
-const projects = [ {_id: "234iuhowef9803rh", title: "Big Ben" }, {_id: "584pduablf9571qt", title: "London Bridge" } ]
+import ProjectService from "../../Services/ProjectService";
+
 export default function ProjectList(props) {
+  const [projects, setProjects] = useState([]);
+
+  const getProjects = async () => {
+    try {
+      const allProjects = await ProjectService.getAllProjects();
+      const Model = allProjects.data.projectsData;
+      setProjects(Model);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getProjects();
+  }, []);
+
   return (
     <div className="projectList">
       <List className="list">
         {projects.map((project) => (
-          <a href={"project/" + project._id}>
+          <a href={"project/" + project._id} key={project._id}>
             <LinkItem className="listItem btn btn-primary" key={project._id}>
-            {project.title}
+              {project.title}
             </LinkItem>
           </a>
         ))}
