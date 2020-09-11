a92c763f675f78c78cf198659052845bc08cab7f
meeday <59993824+meeday@users.noreply.github.com>
2020-09-12 00:26:31 +0100

    List project rendered

--- src/components/ProjectsList/ProjectsList.js
+++ src/components/ProjectsList/ProjectsList.js
@@ -3,48 +3,41 @@ import { List, LinkItem } from "../List/List";
 import "./ProjectsList.css";
 import ProjectService from "../../Services/ProjectService";
 import { AuthContext } from "../../Context/AuthContext";
+import { ProjectContext } from "../../Context/ProjectContext";
 
 export default function ProjectList(props) {
-  const authContext = useContext(AuthContext);
-  const id= (((authContext || {}).userInfo || {}).user || {})._id || null;
-  
-  const [projects, setProjects] = useState([]);
-  const [project, setProject] = useState(null)
+  const { IdValue } = useContext(AuthContext);
+  const { projectInfo, setProjectInfo } = useContext(ProjectContext);
 
-  const userProjects = async (id) => {
+  const userProjects = async () => {
     try {
-      const userProjectData =   await ProjectService.getProjectByUserId(id);
-      console.log(userProjectData);      
+      const { data } = await ProjectService.getProjectByUserId(IdValue.userId);
+      setProjectInfo(data.usersProjects);
     } catch (error) {
       console.log(error);
     }
-  }
-
-  const getProjects = async () => {
-    try {
-      const allProjects = await ProjectService.getAllProjects();
-      // console.log(allProjects);
-      const Model = allProjects.data.projectsData;
-      setProjects(Model);
-    } catch (error) {
-      console.log(`Error - ProjectsList.js - getProjects() - ${error}`);
-    }
   };
+
   useEffect(() => {
-    getProjects();
-    userProjects()
+    userProjects();
   }, []);
+  console.log(projectInfo);
 
   return (
     <div className="projectList">
       <List className="list">
-        {projects.map((project) => (
-          <a href={"project/" + project._id} key={project._id}>
-            <LinkItem className="listItem btn btn-primary" key={project._id}>
-              {project.title}
-            </LinkItem>
-          </a>
-        ))}
+        {projectInfo
+          ? projectInfo.map((project) => (
+              <a href={"project/" + project._id} key={project._id}>
+                <LinkItem
+                  className="listItem btn btn-primary"
+                  key={project._id}
+                >
+                  {project.title}
+                </LinkItem>
+              </a>
+            ))
+          : null}
       </List>
       <div className="text-center">
         <button className="btn btn-primary add">
