4792a3cfa9863919ed919b88b587cb4d021ee204
meeday <59993824+meeday@users.noreply.github.com>
2020-09-27 03:04:54 +0100

    projectList updated

--- src/components/ProjectsList/ProjectsList.js
+++ src/components/ProjectsList/ProjectsList.js
@@ -1,24 +1,22 @@
 import React, { useContext } from "react";
 import { List, LinkItem } from "../List/List";
 import "./ProjectsList.css";
-import { AuthContext } from "../../Context/AuthContext";
 import { UserContext } from "../../Context/UserContext";
 
 export default function ProjectList(props) {
-  const authContext = useContext(AuthContext);
   const userContext = useContext(UserContext);
-  const projectList = userContext.userProjects;
+  const { userProjects } = userContext;
 
   return (
     <div className="projectList">
       <List className="list">
-        {projectList.map((project) => (
+        {userProjects ? (userProjects.map((project) => (
           <a href={"project/" + project._id} key={project._id}>
             <LinkItem className="listItem btn btn-primary" key={project._id}>
               {project.title}
             </LinkItem>
           </a>
-        ))}
+        ))): (null)}
       </List>
       <div className="text-center">
         <button className="btn btn-primary add">
