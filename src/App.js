0e7b225068f945fba93d095850b6f2fda8016350
meeday <59993824+meeday@users.noreply.github.com>
2020-09-27 03:00:48 +0100

    authentication used in useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import AuthService from "./Services/AuthService";
 import UserForm from "./components/UserForm/UserForm";
 import Login from "./components/pages/Login/Login";
 import Register from "./components/pages/Register/Register";
@@ -12,30 +18,66 @@ import AccountDashboard from "./components/pages/AccountDashboard/AccountDashboa
 import "./App.css";
 
 function App(props) {
+  const [token, setToken] = useState("");
+  const [authed, setAuthed] = useState();
+  useEffect(() => {
+    const checkAuth = async () => {
+      if (token === null) {
+        localStorage.setItem("x-auth-token", "");
+      } else {
+        const userToken = localStorage.getItem("x-auth-token");
+        setToken(userToken);
+      }
+      const { data } = await AuthService.isAuthenticated(token);
+      setAuthed(data);
+    };
+    checkAuth();
+  }, [token]);
+
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <Switch>
-          <Route exact path="/login" render={() => (
-            <UserForm>
-                <Login history={props.history}/>
+          <Route
+            exact
+            path="/login"
+            render={() => (
+              <UserForm>
+                <Login history={props.history} />
               </UserForm>
             )}
-            />
-          <Route exact path="/register" render={() => (
-            <UserForm>
+          />
+          <Route
+            exact
+            path="/register"
+            render={() => (
+              <UserForm>
                 <Register />
               </UserForm>
             )}
-          />                  
-            <Route exact path="/" component={Home} />
-            <Route exact path="/new-project" component={NewProject} />
-            <Route exact path="/project/:projectId" component={Project} />
-            <Route exact path="/project/:projectId/new-risk" component={NewRisk} />
-            <Route exact path="/project/:projectId/risk/:riskId" component={ExistingRisk} />
-            <Route exact path="/account" component={AccountDashboard} />
+          />
+          {authed ? (
+            <>
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/new-project" component={NewProject} />
+              <Route exact path="/project/:projectId" component={Project} />
+              <Route
+                exact
+                path="/project/:projectId/new-risk"
+                component={NewRisk}
+              />
+              <Route
+                exact
+                path="/project/:projectId/risk/:riskId"
+                component={ExistingRisk}
+              />
+              <Route exact path="/account" component={AccountDashboard} />
+            </>
+          ) : (
+            <Redirect to="/login" />
+          )}
         </Switch>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
